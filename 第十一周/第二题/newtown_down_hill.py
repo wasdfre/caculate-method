c1b0ada5e49e06d80c3e97ea632655f64c16f444
wasdfre <2247246336@qq.com>
2022-05-19 20:34:41 +0800

    二分+牛顿

--- 第十一周/第二题/newtown_down_hill.py
+++ 第十一周/第二题/newtown_down_hill.py
@@ -11,12 +11,15 @@ def two_divide(left,right,a):
     if(not f(left,a) or not f(right,a)):
         return  left if  not f(left,a)  else right
     error=0.1;
-    while(abs(left-right)>error):
+    mid=(left+right)/2;
+    while(f(mid,a)>error):
         mid=(left+right)/2;
         if(f(left,a)*f(mid,a)<0):
             right=mid;
         else:
             left=mid;
+        # print(mid);
+    
     return (right+left)/2;
 
 def newtown_down_hill(left,right,a):
@@ -48,7 +51,7 @@ def newtown_down_hill(left,right,a):
     return xi;
 
 #当系数为a时计算计算K个根\
-K=20;
+K=1000;
 a=-1;
 #小区间长度,但感觉pi不好算，换个整数？
 # h=3;
@@ -65,8 +68,8 @@ for i in range(0,K):
     # number_iter+=iter_temp;
     i=i+1;
     ##更新边界  
-    #边界的参数
-    left=right;right=right+h;    
-   # print(2222,left,right);
+    #边界的参数，左开右闭
+    right=right+h; left=right-h+1e-14;  
+    # print(2222,left,right);
 
 print("零点为",x);
